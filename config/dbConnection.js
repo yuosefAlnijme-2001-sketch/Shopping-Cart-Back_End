d67b299e9c3ea9f637e65587a1481b0c849d4004
yuosefAlnijme-2001-sketch <yuosefalnijmejop@gmail.com>
2026-03-02 10:36:43 +0200

    fix start script

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
-const DBConnection = () => {
-  mongoose.connect(process.env.DB_URL).then((connect) => {
-    console.log(`DB Connection with host ${connect.connection.host}`);
-  });
+const DBConnection = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.DB_URL);
+    console.log(`DB Connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.error("DB Error:", error.message);
+    process.exit(1); // مهم عشان يوضح المشكلة
+  }
 };
 
 module.exports = DBConnection;
